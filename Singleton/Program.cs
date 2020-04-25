a3a5eb91d6b24a8114c71042e048c9774bc9e490
arslanengin <enginarslan48@gmail.com>
2020-04-25 17:29:18 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class CustomerManager
+    {
+        private CustomerManager()
+        {
+
+        }
+
+
+    }
+}
