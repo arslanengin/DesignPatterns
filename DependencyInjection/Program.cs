0bcbd9083e1d8d0986e991bedf7f052cbc83cb48
arslanengin <enginarslan48@gmail.com>
2020-04-26 03:06:55 +0300

    Added DependencyInjection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DependencyInjectionWithoutSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            productManager.Save();
+        }
+    }
+
+    class EFProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Save()
+        {
+
+            //Business Code
+            EFProductDal productDal = new EFProductDal();
+            productDal.Save();
+        }
+    }
+
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Singleton
+namespace ThreadSafeSingletonPattern
 {
     class Program
     {
